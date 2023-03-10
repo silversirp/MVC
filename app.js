0b33fb730b73de30dca5bb45e1699404d1569278
Silver Sirp <silver.sirp@khk.ee>
2023-03-10 15:44:55 +0200

    As a user, I want to see a list of items [Delivered #184603907]

--- app.js
+++ app.js
@@ -18,18 +18,6 @@ db.run('CREATE TABLE IF NOT EXISTS Item (id INTEGER PRIMARY KEY, name TEXT, imag
     db.run('INSERT INTO Item (name, image, description) VALUES (?, ?, ?)', ['Item 2', 'image2.jpg', 'Description for item 2']);
     db.run('INSERT INTO Item (name, image, description) VALUES (?, ?, ?)', ['Item 3', 'image3.jpg', 'Description for item 3']);
 
-    // To test error 404
-    // db.run('DROP TABLE Item');
-
-    // // Query the list of items
-    // db.all('SELECT name, image, description FROM items', (err, rows) => {
-    //     if (err) {
-    //         console.error(err.message);
-    //         return;
-    //     }
-    //     // Log the results to the console
-    //     console.log(rows);
-    // });
 });
 
 // Close the database connection when done
@@ -46,7 +34,13 @@ app.get('/', (req, res) => {
 // Set up a route to render the "List of items" page /items using Pug and retrieve a list of items from a database (itemsdb.sqlite) and display in a table
 app.get('/items', async (req, res) => {
     const items = await prisma.item.findMany(); // retrieve list of items from database
-    res.render('/items/index', { items }); // render the "items" template and pass in the list of items
+
+    // If there are no items in the database, the backend returns a 404 Not Found response with a message "No items found".
+    if (items.length === 0) {
+        return res.status(404).send('No items found');
+    }
+
+    res.render('items', { items: items }); // render the "items" template and pass in the list of items
 });
 
 
